ca8128847ae2c5c3dcfd6bc2b19fbd47d0648e65
Ward8-py <warddeeb822@gmail.com>
2026-06-25 05:26:43 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> answer;
+        unordered_map<string, vector<string>> table;
+        
+        for(int i=0; i<strs.size();i++){
+            string key=strs[i];
+            sort(key.begin(),key.end());
+            table[key].push_back(strs[i]);
+
+        }
+
+        for(auto &j:table){
+            answer.push_back(j.second);
+        }
+        return answer;
+
+
+
+
+    }
+};
