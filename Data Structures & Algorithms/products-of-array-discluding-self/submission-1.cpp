cd89ba2632544fa6a9731ae4faa8d0dffb68190c
Ward8-py <warddeeb822@gmail.com>
2026-09-20 21:43:15 +0300

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> prefix(nums.size());
+        vector<int> postfix(nums.size());
+        vector<int> answer;
+        postfix[nums.size()-1]=nums[nums.size()-1];
+        prefix[0]=nums[0];
+    
+        for(int i=1; i<nums.size();i++){
+            prefix[i]=prefix[i-1]*nums[i];
+            
+        }
+        for(int i=nums.size()-2;i>=0;i--){
+            postfix[i]=postfix[i+1]*nums[i];
+        }
+        for(int i=0; i<nums.size();i++){
+            //answer.push_back(prefix[i-1]*postfix[i+1]);
+            if(i==0){
+                answer.push_back(postfix[i+1]);
+            }
+            else if(i==nums.size()-1){
+                answer.push_back(prefix[i-1]);
+            }
+            else{
+                answer.push_back(prefix[i-1]*postfix[i+1]);
+            }
+        }
+        
+        return answer;
+    }
+};
